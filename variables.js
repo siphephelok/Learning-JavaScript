6a0a9e6f056d8530171b83b371def7c21f701194
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-09 18:58:55 +0200

    Added some variables

--- variables.js
+++ variables.js
@@ -1,3 +1,11 @@
 //A variable is just a placeholder, you can store anyvalue
-var firstName = "Siphephelo"
+var firstName = 'Siphephelo'
+var age = 11
+var isFemale = true
+var balance = 100.32
+var dob = new Date(2023, 8, 9)
 console.log(firstName)
+console.log(age)
+console.log(isFemale)
+console.log(balance)
+console.log(dob)
