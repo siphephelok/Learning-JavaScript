ccc2ae6ede73060188bffc38f991eb382e881cd9
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-10 10:31:35 +0200

    Accessing OBJECTS

--- strings.js
+++ strings.js
@@ -1,7 +1,21 @@
 //Objects is a collection of properties, each property has a value
 
-var jobTitle = "Software Developer"
-var age = 15
-var isMale = true
-var person = {} //This is an empty object
+var person = {
+    firstName: 'Siphephelo', 
+    age: 15,
+    isMale: true,
+    dob: new Date(2000, 1 , 27).toJSON(),
 
+    //Nested objects
+
+    address: {
+        city: 'Durban',
+        postCode: 'SW9'
+    }
+} 
+
+//Accessing Objects
+
+console.log(person)
+console.log(person.firstName)
+console.log(person.address.city)
