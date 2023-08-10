5456254fd510e735e2016f6ba9b4e7c5e8f2abb5
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-10 10:44:21 +0200

    Flipping values

--- boolean.js
+++ boolean.js
@@ -1,9 +1,12 @@
 //Boolean 
 var isAdult = true
 
-if(isAdult){
-    console.log("is an adult :)")
-}
-else{
+if(!isAdult){
     console.log("is not an adult :(")
 }
+else{
+    
+    console.log("is an adult :)")
+}
+
+console.log(!true) //flipping the values
