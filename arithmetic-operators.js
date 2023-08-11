55dce89a762d78475ba2805eb11c09db0d6ffa10
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-11 11:15:03 +0200

    Basic operators

---
+++ arithmetic-operators.js
@@ -0,0 +1,13 @@
+//Arithmetic operators
+var addition = 2 + 2
+var subtraction = 2 - 2 
+var division = 2 / 2 
+var multiplication = 2 * 2
+var remainder = 2 % 2 
+var exponentation = 3 ** 4 
+
+console.log(addition)
+console.log(subtraction)
+console.log(division)
+console.log(multiplication)
+console.log(remainder)
