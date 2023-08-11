b7ade4b03a0ef6d110aad89c214684c9b61edba0
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-11 11:21:28 +0200

    function created

---
+++ functions.js
@@ -0,0 +1,8 @@
+//Functions is a set of statements that perform that a task
+
+function addNumbers(number1, number2){
+    var addition = number1 + number2
+    console.log(addition)
+}
+
+addNumbers(4, 5)
